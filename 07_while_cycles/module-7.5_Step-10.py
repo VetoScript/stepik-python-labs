e1ea46485aed1a784d93ccd96dfdfac006ccdec9
VetoScript <75677625+VetoScript@users.noreply.github.com>
2026-05-09 21:51:53 +0300

    Update module-7.5_Step-10.py
    Calculate and display time in hours and minutes for a range.

--- 07_while_cycles/module-7.5_Step-10.py
+++ 07_while_cycles/module-7.5_Step-10.py
@@ -2,8 +2,10 @@ hour1 = int(input())
 minute1 = int(input())
 hour2 = int(input())
 minute2 = int(input())
+
 start_minutes = hour1 * 60 + minute1
 end_minutes = hour2 * 60 + minute2
+
 while start_minutes <= end_minutes:
     h = start_minutes // 60
     m = start_minutes % 60
