a11111d37c5150dab03ac92045f40bde44d49997
Andrey Lopatin <kwc00k1ekw@gmail.com>
2026-05-09 21:49:23 +0300

    feat: implement time normalization and loop for interval output

---
+++ 07_while_cycles/module-7.5_Step-09.py
@@ -0,0 +1,20 @@
+price = int(input())
+coins = 0
+
+while price >= 25:
+    coins += 1
+    price -= 25
+
+while price >= 10:
+    coins += 1
+    price -= 10
+
+while price >= 5:
+    coins += 1
+    price -= 5
+
+while price >= 1:
+    coins += 1
+    price -= 1
+
+print(coins)
